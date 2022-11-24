a8382d34b81b78c61eae503fd913a5185dfe5d3d
squidman7 <akito807233@gmail.com>
2022-11-24 09:18:55 +0900

    Cがキャラ移動処理を追加

--- game1/A.java
+++ game1/A.java
@@ -14,6 +14,9 @@ public class A extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "down" ) ){
+            setRotation(90);
+            move(1);
+        }
     }    
 }
