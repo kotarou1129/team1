40f5fcae515939bb703c70a73203d104e334bfed
AkaishiKazuto <18002@hakodate.kosen-ac.jp>
2022-12-08 10:05:22 +0900

    kirinuki追加

--- game1/MyWorld.java
+++ game1/MyWorld.java
@@ -17,7 +17,5 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1700, 900, 1);
-        addObject( new シャギ様(), 300, 200 );
-        addObject( new farmer(), 300, 100 );
     }
 }
