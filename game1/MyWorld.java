75eb466cb7e3eeffded04ef8f58a4656147eaccd
squidman7 <akito807233@gmail.com>
2022-12-08 09:54:44 +0900

    Cが背景の作成

--- game1/MyWorld.java
+++ game1/MyWorld.java
@@ -16,7 +16,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(1700, 900, 1);
         addObject( new シャギ様(), 300, 200 );
         addObject( new farmer(), 300, 100 );
     }
