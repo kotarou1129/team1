7b369a1cef1c1e6b386bcb860b5306e5d466bf02
squidman7 <akito807233@gmail.com>
2022-12-08 09:38:47 +0900

    Cがキャラの動作作成

--- game1/A.java
+++ game1/A.java
@@ -14,12 +14,12 @@ public class A extends Actor
      */
     public void act() 
     {
-<<<<<<< HEAD
+
         if( Greenfoot.isKeyDown( "down" ) ){
             setRotation(90);
             move(1);
         }
-=======
+
         if( Greenfoot.isKeyDown( "up" ) ){
         setRotation(-90);
         move(5);
@@ -29,6 +29,5 @@ public class A extends Actor
         move(5);
         }
         // Add your action code here.
->>>>>>> 82dd511651d30bfb1bfa9c32ee3a053054cfc40c
     }    
 }
