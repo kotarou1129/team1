ab38247b0b294fac71cb88670b88f5b6fff6041e
kotarou1129 <18080@hakodate.kosen-ac.jp>
2022-12-08 10:00:33 +0900

    Aがシャギ様を表示、移動

--- game1/MyWorld.java
+++ game1/MyWorld.java
@@ -17,6 +17,5 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        addObject( new シャギ様(), 300, 200 );
     }
 }
