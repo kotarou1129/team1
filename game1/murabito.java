67a8f7f764df0459afabe2d5ad326b7b527c75d8
AkaishiKazuto <18002@hakodate.kosen-ac.jp>
2022-12-08 10:12:07 +0900

    kirinuki追加

--- game1/murabito.java
+++ game1/murabito.java
@@ -14,21 +14,21 @@ public class murabito extends Actor
      */
     public void act() 
     {
-            if( Greenfoot.isKeyDown( "right" ) ){
-        setRotation(180);
-        move(4);
-    }// Add your action code here.
-            if( Greenfoot.isKeyDown( "left" ) ){
+        if( Greenfoot.isKeyDown( "right" ) ){
         setRotation(0);
         move(4);
-    }
-            if( Greenfoot.isKeyDown( "up" ) ){
-        setRotation(90);
-        move(4);
-    }
-            if( Greenfoot.isKeyDown( "down" ) ){
+        }
+        if( Greenfoot.isKeyDown( "left" ) ){
+        setRotation(0);
+        move(-4);
+        }
+        if( Greenfoot.isKeyDown( "up" ) ){
         setRotation(-90);
         move(4);
-    }/// Add your action code here.
+        }
+        if( Greenfoot.isKeyDown( "down" ) ){
+        setRotation(90);
+        move(4);
+        }/// Add your action code here.
     }    
 }
