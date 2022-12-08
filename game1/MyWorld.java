d7ad67b9e9d23b52b94dbada579dd94baad0b03d
squidman7 <akito807233@gmail.com>
2022-12-08 09:42:49 +0900

    Cがキャラの動作作成

--- game1/MyWorld.java
+++ game1/MyWorld.java
@@ -18,5 +18,6 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         addObject( new シャギ様(), 300, 200 );
+        addObject( new farmer(), 300, 100 );
     }
 }
