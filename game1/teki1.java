15b96a125ece09c2aab79900afa4329d3f950b1c
squidman7 <akito807233@gmail.com>
2022-12-22 10:22:08 +0900

    Cが敵キャラのランダム移動を追加

--- game1/teki1.java
+++ game1/teki1.java
@@ -12,30 +12,18 @@ public class teki1 extends Actor
      * Act - do whatever the teki1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
-    {
-      int motime=1;
-      int A = 0;
-      int B = 360;
-      int C = A + (int)(Math.random()*((B-A)+1));
-      setRotation(C);
-      for(int cnt=0; cnt<10000000; cnt++){
-          if(motime%250001==0){
-              move(1);
-              motime=1;
-            }
-          motime++;
-        }
-      
-
-        
-        
-      /*Actor actor = getOneIntersectingObject( teki1.class );
-      if( actor != null ){
+    static int  cnt=0;
     
-          getWorld().showText( "GAME OVER", 100, 50 );
-          Greenfoot.stop();
-          
-          }*/
+    public void act() 
+    {  
+      move(2);
+      if(cnt%40==0){
+              int A = 0;
+              int B = 360;
+              int C = A + (int)(Math.random()*((B-A)+1));
+              setRotation(C);
+              cnt=1;
+      }
+      cnt++;
     }    
 }
--- game1/teki2.java
+++ game1/teki2.java
@@ -12,19 +12,18 @@ public class teki2 extends Actor
      * Act - do whatever the teki2 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    static int  cnt=0;
     public void act() 
     {
-      int motime=1;
-      int A = 0;
-      int B = 360;
-      int C = A + (int)(Math.random()*((B-A)+1));
-      setRotation(C);
-      for(int cnt=0; cnt<10000000; cnt++){
-          if(motime%250001==0){
-              move(1);
-              motime=1;
-            }
-          motime++;
-        }// Add your action code here.
+      move(3);
+      if(cnt%40==0){
+              int A = 0;
+              int B = 360;
+              int C = A + (int)(Math.random()*((B-A)+1));
+              setRotation(C);
+              cnt=1;
+      }
+      cnt++;
     }    
 }
